983d99adbbc4f968c7a7e5cdc31aa9615d0ee8fe
Gabriela Gama <gabycgama@hotmail.com>
2021-06-11 23:02:44 -0300

    1-day: initial app interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
-import Head from "next/head";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheça nossos profissionais"}
+        subTitle={
+          "Preencha seu endereço e veja todos os profissionais da sua região"
+        }
+      />
+      <UserInformation
+        name={"Gabriela"}
+        picture={"https://github.com/gabicgama.png"}
+        rating={5}
+        description={"Minas Gerais"}
+      />
+    </div>
+  );
 }
